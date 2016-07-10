356691133245f4f57f82077060dff509d9bdf4d7
Roy Portas <royportas@gmail.com>
2016-07-10 19:22:01 +1000

    Updated install

--- setup_simlinks.sh
+++ setup_simlinks.sh
@@ -1,6 +1,11 @@
 # Run this script within the user's home directory
 # For example 'sh dotfiles/setup_simlinks.sh'
 
-ln -s dotfiles/vimrc .vimrc
-ln -s dotfiles/bashrc .bashrc
-ln -s dotfiles/tmux.conf .tmux.conf
+ln -s `pwd`/vimrc ~/.vimrc
+ln -s `pwd`/bashrc ~/.bashrc
+ln -s `pwd`/tmux.conf ~/.tmux.conf
+
+# Clone vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Dotfiles setup, run ':PluginInstall' within Vim to setup plugins"
