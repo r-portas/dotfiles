a19a7c1893880a74c77ebc4ae4cabc406f00dd0c
Roy Portas <royportas@gmail.com>
2016-08-29 21:01:13 +1000

    Added node apps

--- install-node-modules.sh
+++ install-node-modules.sh
@@ -1,5 +1,8 @@
+# Usefully Node CLI applications
+
 npm install -g http-server
 npm install -g speed-test
 npm install -g vtop
 npm install -g iponmap
 npm install -g live-server
+npm install -g webtorrent-cli
