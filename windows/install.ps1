70d548fa9e9b4ed7dd72d45285bd47c1fed8eaaa
Roy Portas <royportas@gmail.com>
2018-07-20 10:47:25 +1000

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,8 +1,11 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# Git for windows
+choco install -y git
+
 choco install -y googlechrome
 choco install -y firefox
-choco install -y nodejs.install
-choco install -y 7zip.install
+choco install -y nodejs
+choco install -y 7zip
 choco install -y python3
 choco install -y f.lux
