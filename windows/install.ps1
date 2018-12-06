3f90adc933ecc157c5acc3f581aaaabc8cbaeb93
Roy Portas <royportas@gmail.com>
2018-12-07 07:59:11 +1000

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,11 +1,21 @@
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Git for windows
 choco install -y git
 
+# Web Browsers
 choco install -y googlechrome
-choco install -y firefox
-choco install -y nodejs
-choco install -y 7zip
+
+# Languages
+choco install -y python
 choco install -y python3
-choco install -y f.lux
+choco install -y nodejs
+choco install -y dotnetcore-sdk
+
+# Web Dev tools
+choco install -y yarn
+npm install --global --production windows-build-tools
+
+# Tools
+choco install -y docker-for-windows
+choco install -y 7zip
